ba6a90c727c0a3d842b8d307b4828020f68514c5
Victor Halim <74048799+victorhalimm@users.noreply.github.com>
2025-07-28 12:40:51 +0700

    Time: 2 ms (12.64%), Space: 8.7 MB (83.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
         stack<char> stack;
 
         for (char c : s) {
             if (c == '(' || c == '[' || c == '{') {
                 stack.push(c);
-            } else {
-                if (stack.empty()) return false;
-
-                if (stack.top() != '(' && c == ')') {
-                    return false;
-                }
-
-                else if (stack.top() != '[' && c == ']') {
-                    return false;
-                }
-
-                else if (stack.top() != '{' && c == '}') {
-                    return false;
-                }
-
-                stack.pop();
+                continue;
             }
 
+            if (stack.empty()) return false;
+
+            if (c == ')' && stack.top() == '(') {
+                stack.pop();
+                continue;
+            }
+            if (c == ']' && stack.top() == '[') {
+                stack.pop();
+                continue;
+            }
+            if (c == '}' && stack.top() == '{') {
+                stack.pop();
+                continue;
+            }
+
+            return false;
+            
         }
 
-
         return stack.empty();
-
     }
 };
