d83a152153127638b74dda4ced47d18dd0f9f418
Victor Halim <74048799+victorhalimm@users.noreply.github.com>
2025-08-06 10:18:10 +0700

    Time: 21 ms (23.33%), Space: 14.2 MB (37.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,17 @@
-
-#include <unordered_map> 
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> hash_map;
-        int sol = 0;
-        int start = 0; 
-
-        for (int i = 0; i < s.length(); ++i) {
-            char c = s[i];
-
-            if (hash_map.find(c) != hash_map.end() && hash_map[c] >= start) {
-                start = hash_map[c] + 1;
+        unordered_set<char> set;
+        int l = 0, longest = 0;
+        for (int r = 0; r < s.size(); r ++) {
+            while (set.count(s[r])) {
+                set.erase(s[l ++]);
             }
 
-            hash_map[c] = i;
-
-            sol = max(sol, i - start + 1);
+            set.insert(s[r]);
+            longest = max(longest, r - l + 1);
         }
 
-        return sol;
+        return longest;
     }
 };
