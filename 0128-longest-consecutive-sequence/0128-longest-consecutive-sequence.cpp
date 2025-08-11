bed0d3ce56546481800ef93948bfb27fd1ec8666
Victor Halim <74048799+victorhalimm@users.noreply.github.com>
2025-08-11 12:53:15 +0700

    Time: 91 ms (31.05%), Space: 88.9 MB (28.22%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,48 +1,23 @@
-#include <vector>
-#include <unordered_set>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-
-    int max(int a, int b) {
-        return a > b ? a : b;
-    }
-
     int longestConsecutive(vector<int>& nums) {
-        
+        unordered_set<int> set;
 
-        unordered_set<int> hash_set;
-        unordered_set<int> curr_set;
-
-        int longest = 0;
-
-        for (int num : nums) {
-            hash_set.insert(num);
+        for (int num: nums) {
+            set.insert(num);
         }
 
-        for (int num : nums) {
-
-            if (hash_set.find(num - 1) == hash_set.end()) {
-
-                int streak = 1;
-                int curr = num;
-
-                while (hash_set.find(curr + 1) != hash_set.end()) {
-                    streak ++;
-                    curr ++;
+        int longest = 0;
+        for (int num: set) {
+            if (!set.count(num - 1)) {
+                int next = num, curr = 0;
+                while (set.count(next)) {
+                    longest = max(longest, ++curr);
+                    next ++;
                 }
-
-                longest = max(streak, longest);
-
             }
-
         }
 
         return longest;
     }
 };
-
-// 102 1 3 2 101 5 4 103
