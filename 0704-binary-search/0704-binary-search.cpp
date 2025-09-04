aa8b8a4290b0acde68e0c59e98e595f735c1b1cb
Victor Halim <74048799+victorhalimm@users.noreply.github.com>
2025-09-04 17:12:59 +0700

    Time: 0 ms (100%), Space: 31.3 MB (79.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,28 +1,20 @@
-#include <vector>
-
-
-using namespace std;
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
         int l = 0, r = nums.size() - 1;
 
-
         while (l <= r) {
-            int mid = l + (r - l) / 2;
+            if (nums[l] == target) return l;
+            if (nums[r] == target) return r;
 
-            if (nums[mid] == target) return mid;
+            int m = l + (r - l) / 2;
 
-            else if (nums[mid] < target) l = mid + 1;
+            if (nums[m] == target) return m;
 
-            else r = mid - 1;
-
+            else if (target > nums[m]) l = m + 1;
+            else r = m - 1;
         }
 
         return -1;
-
-
     }
 };
