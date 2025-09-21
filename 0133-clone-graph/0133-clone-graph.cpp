2d92923c1edb181399cf0c35b67a5054f91caab5
Victor Halim <74048799+victorhalimm@users.noreply.github.com>
2025-09-21 17:40:54 +0700

    Time: 3 ms (67.46%), Space: 11.9 MB (89.29%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    Node* dfs(unordered_map<Node*, Node*>& cloneMap, Node* node) {
+        if (cloneMap[node]) return cloneMap[node];
+
+        cloneMap[node] = new Node(node->val);
+
+        for (Node* neighbor: node->neighbors) {
+            Node* newNeighbor = cloneMap[neighbor] ? cloneMap[neighbor] : dfs(cloneMap, neighbor);
+            cloneMap[node]->neighbors.push_back(newNeighbor);
+        }
+
+        return cloneMap[node];
+    }
+
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) return nullptr;
+
+        unordered_map<Node*, Node*> cloneMap;
+
+        return dfs(cloneMap, node);
+    }
+};
