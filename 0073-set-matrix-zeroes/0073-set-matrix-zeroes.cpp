527a0e03120d046e61d48430da4abb378c047e20
Victor Halim <74048799+victorhalimm@users.noreply.github.com>
2025-10-01 16:15:50 +0700

    Time: 0 ms (100%), Space: 18.5 MB (95.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,58 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int> zeroRow, zeroCol;
-
-        int m = matrix[0].size();
         int n = matrix.size();
+        int m = matrix[0].size();
 
-        for (int i = 0; i < n; i ++) {
-            for (int j = 0; j < m; j ++) {
+        bool firstRowZero = false, firstColZero = false;
+
+        // Check if first row needs to be zero
+        for (int j = 0; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if first col needs to be zero
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use first row/col as markers
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    zeroRow.insert(i);
-                    zeroCol.insert(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (auto row: zeroRow) {
-            for (int i = 0; i < m; i ++) matrix[row][i] = 0;
+        // Zero cells based on markers
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
         }
 
-        for (auto col: zeroCol) {
-            for (int i = 0; i < n; i ++) matrix[i][col] = 0;
+        // Zero out first row if needed
+        if (firstRowZero) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out first col if needed
+        if (firstColZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 };
